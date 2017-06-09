f35241566ec8c65de763c43f71e27a665104e19b
Steve Horsfield <stevehorsfield@users.noreply.github.com>
2017-06-09 11:45:24 +0200

    Allow traffic from ELBs to TFE (#7)

--- modules/mdl_extensions/security-terraform-enterprise-instance.tf
+++ modules/mdl_extensions/security-terraform-enterprise-instance.tf
@@ -15,3 +15,21 @@ resource "aws_security_group_rule" "terraform-enterprise-instance-ssh" {
   protocol          = "tcp"
   cidr_blocks       = ["${var.vpn_access_cidr}"]
 }
+
+resource "aws_security_group_rule" "terraform-enterprise-instance-internal-elb" {
+  security_group_id = "${aws_security_group.terraform-enterprise-instance.id}"
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = "${aws_security_group.portal-internal.id}"
+}
+
+resource "aws_security_group_rule" "terraform-enterprise-instance-public-elb" {
+  security_group_id = "${aws_security_group.terraform-enterprise-instance.id}"
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = "${aws_security_group.portal-public.id}"
+}
